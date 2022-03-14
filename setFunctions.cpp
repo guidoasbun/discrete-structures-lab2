7572f7fde85810819c939b77e79b89e52525ccbd
Guido Asbun <guido@asbun.io>
2022-03-13 19:32:35 -0700

    setUnion, setIntersection, setDiff, setSymDiff

--- setFunctions.cpp
+++ setFunctions.cpp
@@ -3,8 +3,9 @@
 #include "set.cpp"
 
 using namespace std;
+
 // Returns a set that is the Union of the two sets s1 and s2
-Set<char> setUnion(const Set<char>& s1, const Set<char>& s2)
+Set<char> setUnion(const Set<char> &s1, const Set<char> &s2)
 {
     Set<char> result;
 
@@ -17,14 +18,14 @@ Set<char> setUnion(const Set<char>& s1, const Set<char>& s2)
     size_t setLengths2 = s2.cardinality();
     for (size_t i = 0; i < setLengths2; ++i)
     {
-            result.insertElement(s2[i]);
+        result.insertElement(s2[i]);
     }
 
     return result;
 }
 
 // Returns a set that is the intersection of the two sets s1 and s2
-Set<char> setIntersection(const Set<char>& s1, const Set<char>& s2)
+Set<char> setIntersection(const Set<char> &s1, const Set<char> &s2)
 {
     Set<char> result;
 
@@ -39,7 +40,7 @@ Set<char> setIntersection(const Set<char>& s1, const Set<char>& s2)
 }
 
 // Returns a set that is the s1 - s2
-Set<char> setDiff(const Set<char>& s1, const Set<char>& s2)
+Set<char> setDiff(const Set<char> &s1, const Set<char> &s2)
 {
     Set<char> result;
     size_t setLengths1 = s1.cardinality();
@@ -54,31 +55,43 @@ Set<char> setDiff(const Set<char>& s1, const Set<char>& s2)
 }
 
 // Returns a set that is the symetric difference of the two sets s1 and s2
-Set<char> setSymDiff(const Set<char>& s1, const Set<char>& s2)
+Set<char> setSymDiff(const Set<char> &s1, const Set<char> &s2)
 {
     Set<char> result;
 
-    // Add your code here
+    size_t setLengths1 = s1.cardinality();
+    for (size_t i = 0; i < setLengths1; ++i)
+    {
+        if (!(s2.isElement(s1[i])))
+            result.insertElement(s1[i]);
+    }
+
+    size_t setLengths2 = s2.cardinality();
+    for (size_t i = 0; i < setLengths1; ++i)
+    {
+        if (!(s1.isElement(s2[i])))
+            result.insertElement(s2[i]);
+    }
 
     return result;
 }
 
 // Returns true if s1 is a subset of s2
-bool isSubSet(const Set<char>& s1, const Set<char>& s2)
+bool isSubSet(const Set<char> &s1, const Set<char> &s2)
 {
     // Add your code here and return the correct truth value
     return true;
 }
 
 // Returns true if s1 is a proper subset of s2
-bool isProperSubSet(const Set<char>& s1, const Set<char>& s2)
+bool isProperSubSet(const Set<char> &s1, const Set<char> &s2)
 {
     // Add your code here and return the correct truth value
     return true;
 }
 
 // Returns the Power Set of set s as a set of sets
-Set<Set<char>> PowerSet(const Set<char>& s)
+Set<Set<char>> PowerSet(const Set<char> &s)
 {
     Set<Set<char>> result;
 
@@ -88,7 +101,7 @@ Set<Set<char>> PowerSet(const Set<char>& s)
 }
 
 // Returns true if the sets in p make up a Partition of set s
-bool isPartition(const Set<Set<char>>& p, const Set<char>& s)
+bool isPartition(const Set<Set<char>> &p, const Set<char> &s)
 {
     // Add your code here and return the correct truth value
     return true;
